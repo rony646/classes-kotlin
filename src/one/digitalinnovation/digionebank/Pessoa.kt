1fd3185b61e349bd7ae6334bb9e619e5dd5de79d
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-07-19 20:42:06 -0300

    understaing class members

--- src/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,15 +3,16 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Rony"
 
-    var cpf: String = "123.123.123.-61"
+    var cpf: String = "123.123.123-61"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome and $cpf"
 }
 
 fun main() {
     val rony = Pessoa()
 
-
-    println(rony)
-    println(rony.nome)
-    println(rony.cpf)
+    println(rony.pessoaInfo())
 }
