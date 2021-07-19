029d8c9547ae4ab701dc8fcf04bdab0314e6c673
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-07-19 20:34:34 -0300

    setting modifiers

--- src/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Rony"
+
     var cpf: String = "123.123.123.-61"
+    private set
 }
 
 fun main() {
     val rony = Pessoa()
 
+
+    println(rony)
     println(rony.nome)
     println(rony.cpf)
 }
