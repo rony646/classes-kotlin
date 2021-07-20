4cde841060a57a1137b7e57f99233bad02ccb7d6
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-07-20 19:15:26 -0300

    more one test case

--- src/one/digitalinnovation/digionebank/teste/TesteClienteTipoEnum.kt
+++ src/one/digitalinnovation/digionebank/teste/TesteClienteTipoEnum.kt
@@ -4,6 +4,9 @@ import one.digitalinnovation.digionebank.ClienteTipo
 
 fun main() {
     ClienteTipo.values().forEach { elemento ->
-        println("${elemento.name} - $elemento")
+        println("${elemento.name} - ${elemento.descricao}")
     }
+
+    val pf = ClienteTipo.PF
+    println("from val: ${pf.name} - ${pf.descricao}")
 }
